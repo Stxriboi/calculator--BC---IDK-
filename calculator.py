60b6305f343c8fd65ca060340a5a69ce37a129e3
mxxnboii <brycecephus@gmail.com>
2025-04-14 23:07:13 -0400

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,35 +1,48 @@
-# https://github.com/Stxriboi/lab10--BC---IDK-.git
+# https://github.com/Stxriboi/calculator--BC---IDK-
 # Partner 1: Bryce Cephus
-# Partner 2: N/A
+# Partner 2: Bryce Cephus
 
+import math
+
+# Required by autograder
 def add(x, y):
     return x + y
 
-def subtract(x, y):  
+def sub(x, y):
     return x - y
 
-def mul(x, y):  
+def mul(x, y):
     return x * y
 
-def div(x, y):  
+def div(x, y):
     if y == 0:
         raise ValueError("Cannot divide by zero.")
     return x / y
 
-def exp(x, y):  
+def exp(x, y):
     return x ** y
 
 def mod(x, y):
     return x % y
 
 def logarithm(x, base):
-    import math
     return math.log(x, base)
 
 def hypotenuse(a, b):
-    import math
     return math.hypot(a, b)
 
 def square_root(x):
-    import math
     return math.sqrt(x)
+
+# Required by your test_calculator.py
+def subtract(x, y):
+    return sub(x, y)
+
+def multiply(x, y):
+    return mul(x, y)
+
+def divide(x, y):
+    return div(x, y)
+
+def power(x, y):
+    return exp(x, y)
