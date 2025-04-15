645a258bcdb3d5ca9b72c4efac8e21c35a303403
mxxnboii <brycecephus@gmail.com>
2025-04-14 22:54:49 -0400

    calculator.py
    updated

--- calculator.py
+++ calculator.py
@@ -1,19 +1,31 @@
 def add(x, y):
     return x + y
 
-def subtract(x, y):
+def sub(x, y):  
     return x - y
 
-def multiply(x, y):
+def mul(x, y):  
     return x * y
 
-def divide(x, y):
+def div(x, y):  
     if y == 0:
         raise ValueError("Cannot divide by zero.")
     return x / y
 
-def power(x, y):
+def exp(x, y):  
     return x ** y
 
 def mod(x, y):
     return x % y
+
+def logarithm(x, base):
+    import math
+    return math.log(x, base)
+
+def hypotenuse(a, b):
+    import math
+    return math.hypot(a, b)
+
+def square_root(x):
+    import math
+    return math.sqrt(x)
