7a2cd13a817884b0e51f8e54206019b72e6b8288
mxxnboii <brycecephus@gmail.com>
2025-04-14 23:02:54 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
-# https://github.com/Stxriboi/lab10--BC---IDK-.git
+# https://github.com/Stxriboi/calculator--BC---IDK-
 # Partner 1: Bryce Cephus
-# Partner 2: N/A
+# Partner 2: Bryce Cephus
 
 import unittest
 import calculator
@@ -12,19 +12,16 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(calculator.subtract(5, 3), 2)
 
-    def test_mul(self):
+    def test_multiply(self):
         self.assertEqual(calculator.multiply(4, 2), 8)
 
-    def test_div(self):
+    def test_divide(self):
         self.assertEqual(calculator.divide(10, 2), 5)
         with self.assertRaises(ValueError):
             calculator.divide(5, 0)
 
-    def test_pow(self):
+    def test_power(self):
         self.assertEqual(calculator.power(2, 3), 8)
 
     def test_mod(self):
         self.assertEqual(calculator.mod(10, 3), 1)
-
-if __name__ == '__main__':
-    unittest.main()
