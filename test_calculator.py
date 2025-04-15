daee8e4efc01149a9d295fc0e418ba673ad24741
mxxnboii <brycecephus@gmail.com>
2025-04-14 23:08:22 -0400

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-# https://github.com/Stxriboi/calculator--BC---IDK-.git
+# https://github.com/Stxriboi/calculator--BC---IDK-
 # Partner 1: Bryce Cephus
 # Partner 2: Bryce Cephus
 
@@ -9,7 +9,7 @@ class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calculator.add(2, 3), 5)
 
-    def test_subtract(self):
+    def test_sub(self):
         self.assertEqual(calculator.subtract(5, 3), 2)
 
     def test_mul(self):
